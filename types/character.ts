5520f8d8faaf5fc654ba5ebad04bbf33d32149e5
jaewoneee <jaewon810@naver.com>
2023-04-04 15:06:27 +0900

    feat: types

---
+++ types/character.ts
@@ -0,0 +1,20 @@
+export type CharacterProfileType = {
+  CharacterClassName: string;
+  CharacterImage: string;
+  CharacterLevel: number;
+  CharacterName: string;
+  ExpeditionLevel: number;
+  GuildMemberGrade: string;
+  GuildName: string;
+  ItemAvgLevel: string;
+  ItemMaxLevel: string;
+  PvpGradeName: string;
+  ServerName: string;
+  Stats: any[];
+  Tendencies: any[];
+  Title: string;
+  TotalSkillPoint: number;
+  TownLevel: number;
+  TownName: string;
+  UsingSkillPoint: number;
+};
---
+++ types/news.ts
@@ -0,0 +1,15 @@
+export type EventType = {
+  EndDate: string;
+  Link: string;
+  RewardDate: string;
+  StartDate: string;
+  Thumbnail: string;
+  Title: string;
+};
+
+export type NoticeType = {
+  Date: string;
+  Link: string;
+  Title: string;
+  Type: string;
+};
