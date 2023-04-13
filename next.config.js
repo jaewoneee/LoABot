3eaabf2467b3e06100390c54c4e745f844bf359d
jaewoneee <jaewon810@naver.com>
2023-04-13 17:53:26 +0900

    fix:env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  env: {
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    SOCKET: process.env.NEXT_PUBLIC_SOCKET,
+  },
 };
 
 module.exports = nextConfig;
